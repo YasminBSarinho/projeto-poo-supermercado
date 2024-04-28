27f11fcf38dd88687efd10a2dccd88481ca8b28b
Henrique-Arcilio <aldomario.arcilio@outlook.com>
2024-04-28 20:22:21 -0300

    criação do código unico do produto
    Foi implementado no metodo set, duas formas de adicionar o código de um
    produto. Também foi adicionada uma variável de classe, que contará cada
    instancia da classe produto que for feita, usando como base para gerar
    esse código. Exemplo, o produto1 que foi instanciado como primeiro
    produto terá seu código 00001.

--- projeto/produtos/Produto.java
+++ projeto/produtos/Produto.java
@@ -1,18 +1,50 @@
 package projeto.produtos;
 
 public class Produto {
+    private static int quantiaDeInstancias = 0;
     private String codigo;
     private String nome;
     private int unidade;
     private float valorUnitarioDeCompra;
     private float valorUnitarioDeVenda;
 
-    public String getCodigo() {
-        return codigo;
+
+    public Produto(){
+        quantiaDeInstancias += 1;
+        this.setCodigo();
     }
-    public void setCodigo(String codigo) {
+
+    public String getCodigo() {
+       return codigo;
+    }
+
+    public void setCodigo(String codigo){
         this.codigo = codigo;
     }
+
+    public void setCodigo() {
+
+        String idProduto = String.valueOf(Produto.quantiaDeInstancias); 
+
+        switch (idProduto.length()) {
+            case 1:
+                this.codigo = "0000" + Produto.quantiaDeInstancias;
+                break;
+            case 2:
+                this.codigo = "000" + Produto.quantiaDeInstancias;
+                break;  
+            case 3: 
+                this.codigo = "00" + Produto.quantiaDeInstancias;
+                break;
+            case 4:
+                this.codigo = "0" + Produto.quantiaDeInstancias;
+                break;
+            default:
+                this.codigo = "" + Produto.quantiaDeInstancias;
+                break;
+        }
+    }
+
     public String getNome() {
         return nome;
     }
