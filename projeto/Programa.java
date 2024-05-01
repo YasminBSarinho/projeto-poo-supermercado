13f3c2226b6c67666361767787fe6519490b44b4
Henrique Arcilio <135989858+Henrique-Arcilio@users.noreply.github.com>
2024-05-01 09:25:29 -0300

    Arrumada identação e comentários
    Apenas um reajuste da identação e alguns comentários. Nada de novo.

--- projeto/Programa.java
+++ projeto/Programa.java
@@ -6,82 +6,81 @@ import com.google.gson.Gson;
 import java.io.FileWriter;
 import java.io.FileReader;
 
-public class Programa{
-    public static void main(String[] args) {
-    	
-        SistemaMercado sistema = new SistemaMercado();
-        Scanner scanner = new Scanner(System.in);
-    	Gson json = new Gson();
-    	FileWriter escritor;
-    	FileReader leitor;
-    	String cargo;
-    	
-    	while (true){
-    		
-	    	try {
+public class Programa {
+	public static void main(String[] args) {
+
+		SistemaMercado sistema = new SistemaMercado();
+		Scanner scanner = new Scanner(System.in);
+		Gson json = new Gson();
+		FileWriter escritor;
+		FileReader leitor;
+		String cargo;
+
+		while (true) {
+			// Verifica se existe arquivo json para ler
+			try {
 				leitor = new FileReader("sistema.json");
-		        sistema = json.fromJson(leitor, SistemaMercado.class);
-		        leitor.close();
+				sistema = json.fromJson(leitor, SistemaMercado.class);
+				leitor.close();
+			}catch (Exception e) {
+				System.out.println("Não há arquivo por enquanto");
 			}
-	    	catch (Exception e) {
-					System.out.println("Não há arquivo por enquanto");
+
+			// Cadastro ou login inicial
+			try {
+
+				if (sistema.verificarExistenciaDeUsuarios() == false) {
+					System.out.println("Sistema iniciado pela primeira vez, requer o cadastro de gerente");
+					sistema.cadastrarUsuario("gerente");
+					escritor = new FileWriter("sistema.json");
+					String jsonSistema = json.toJson(sistema);
+					escritor.write(jsonSistema);
+					escritor.close();
+
+				} else {
+
+					System.out.print("Digite o seu login: ");
+					String login = scanner.next();
+					System.out.print("Digite o sua Senha: ");
+					String senha = scanner.next();
+
+					cargo = sistema.getCargoUsuarioLogado(login, senha);
+
+					if (cargo != null) {
+						System.out.println("Login efetuado");
+
+					}else {
+						System.out.println("Credenciais informadas são inválidas");
+						continue;
+					}
+
+					// Escolhas
+
+					while (true) {
+						System.out.print("""
+
+									---Opções---
+								[1] - Cadastrar um usuário.
+								[5] - Sair
+									""");
+						int escolha = scanner.nextInt();
+						if (escolha == 5) {
+							break;
+						}
+						sistema.executarEscolha(escolha, cargo);
+
+						escritor = new FileWriter("sistema.json");
+						String jsonSistema = json.toJson(sistema);
+						escritor.write(jsonSistema);
+						escritor.close();
+					}
+
 				}
+			} catch (Exception e) {
+				System.out.println("Erro: " + e.getMessage());
+			}
 
-            try{
-            	
+		}
 
-                if (sistema.verificarExistenciaDeUsuarios() == false){
-                	System.out.println("Sistema iniciado pela primeira vez, requer o cadastro de gerente");
-                    sistema.cadastrarUsuario("gerente");
-                    escritor = new FileWriter("sistema.json");   
-                    String jsonSistema = json.toJson(sistema);
-                    escritor.write(jsonSistema);
-                    escritor.close();
-                    
-                }else{
-                	
-            		System.out.print("Digite o seu login: ");
-            		String login = scanner.next();
-            		System.out.print("Digite o sua Senha: ");
-            		String senha = scanner.next();
-            		 
-        			cargo = sistema.getCargoUsuarioLogado(login, senha);
-        			 
-            		if(cargo != null) {
-            			 System.out.println("Login efetuado");
-
-            		}
-            		else {
-            			System.out.println("Credenciais informadas são inválidas");
-            			continue;
-            		}
-            		
-            		while(true) {
-            			System.out.print("""
-            			 		
-        			 		---Opções--- 
-        			 	[1] - Cadastrar um usuário.
-        			 	[5] - Sair
-        			 		""");
-            			int escolha = scanner.nextInt();
-            			if(escolha == 5) {
-            				break;
-            			}
-            			sistema.executarEscolha(escolha, cargo);
-            			
-                        escritor = new FileWriter("sistema.json");   
-                        String jsonSistema = json.toJson(sistema);
-                        escritor.write(jsonSistema);
-                        escritor.close();
-            		}
-            		
-                }
-            }
-            catch(Exception e){
-            	System.out.println("Erro: " + e.getMessage());
-            }
-            
-    	}
-    	
-    }   
+	}
 }
